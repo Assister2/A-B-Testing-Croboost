b665978e51d327769f0ae6f83c816f7ed0217903
Assister2 <staniwitkow@gmail.com>
2023-10-03 15:42:11 -0700

    feature-testsView-page:Fixed import issues with shadcn components.

--- src/components/Testview/index.tsx
+++ src/components/Testview/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from "@/components/ui/button"
 
 const Testview = () => {
     return (
       <div>
         <h1>New Page Content</h1>
         {/* Add your content here */}
+        <Button>Test</Button>
       </div>
     );
   };
