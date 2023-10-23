fd6e8ce41ba1ce52f65e27454ad413af23f87183
Assister2 <staniwitkow@gmail.com>
2023-10-23 11:39:20 -0700

    resolved merge conflict.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,6 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
   //  output: "hybrid",
   integrations: [react(), tailwind()],
 })
-=======
-  integrations: [react(), tailwind()]
-});
->>>>>>> parent of 031190c (dynamic rendering with id and changes in test ui)
