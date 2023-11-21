ae5a8ec472d1d5025b7aa4fd1b3c850d83b65907
Assister2 <staniwitkow@gmail.com>
2023-11-21 13:38:56 -0600

    reset all cache

--- update_frontend.sh
+++ update_frontend.sh
@@ -2,4 +2,4 @@ BUCKET=ab-website-static
 CLOUDFRONT=E3MUM6UMBNCEGX
 PROFILE=abdev
 npm run build && aws s3 sync ./dist/ s3://$BUCKET/
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT --paths "/" "/dashboard" "/login" "/register" "/tests"
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT --paths "/*"
