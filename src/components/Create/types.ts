b027dbdf5e5bba1c1b52c5c7313a51589e568c15
Assister2 <staniwitkow@gmail.com>
2023-11-28 17:50:48 -0300

    fix route

--- src/components/Create/types.ts
+++ src/components/Create/types.ts
@@ -9,7 +9,6 @@ export interface IExperimentParameters {
   // state: string; // live, staging, inactive
   trigger: string;
   sampleRate: number;
-  description: string;
   Original: TestInputs;
   Variant: TestInputs;
 }
